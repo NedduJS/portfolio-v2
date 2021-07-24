a12dcbcd73cb99c5f1d99e21760cab16b9978dae
NedduJS <nmamanipantoja@gmail.com>
2021-07-23 19:54:18 -0500

    Home styles

--- components/Home.jsx
+++ components/Home.jsx
@@ -5,6 +5,8 @@ import {
   UilGithub,
   UilTwitter,
   UilMessage,
+  UilMouseAlt,
+  UilArrowDown,
 } from '@iconscout/react-unicons';
 
 const Home = () => {
@@ -17,21 +19,46 @@ const Home = () => {
               target='_blank'
               href='https://www.linkedin.com/in/nestoredduardo/'
               className='home'>
-              <UilLinkedin />
+              <UilLinkedin className='home__social-icon' />
             </a>
             <a
               target='_blank'
               href='https://github.com/nestoredduardo'
               className='home'>
-              <UilGithub />
+              <UilGithub className='home__social-icon' />
             </a>
             <a
               target='_blank'
               href='https://twitter.com/nestoredduardo'
               className='home'>
-              <UilTwitter />
+              <UilTwitter className='home__social-icon' />
             </a>
           </div>
+          {/* <div className='home__img'>
+            <svg
+              className='home__blob'
+              viewBox='0 0 200 187'
+              xmlns='http://www.w3.org/2000/svg'
+              xmlnsXlink='http://www.w3.org/1999/xlink'>
+              <mask id='mask0' mask-type='alpha'>
+                <path
+                  d='M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
+        130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
+        97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
+        0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z'
+                />
+              </mask>
+              <g mask='url(#mask0)'>
+                <path
+                  d='M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
+        165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
+        129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
+        -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z'
+                />
+                <image className='home__blob-img' xlinkHref='' />
+              </g>
+            </svg>
+          </div> */}
           <div className='home__data'>
             <h1 className='home__title'>Nestor Mamani</h1>
             <h3 className='home__subtitle'>
@@ -40,11 +67,17 @@ const Home = () => {
             <p className='home__description'>Learning, Building and Sharing</p>
             <a href='#contact' className='button button--flex'>
               Contact Me
-              <UilMessage />
+              <UilMessage className='button__icon' />
             </a>
           </div>
         </div>
-        <div className='home__scroll'></div>
+        <div className='home__scroll'>
+          <a href='#about' className='home__scroll-button button--flex'>
+            <UilMouseAlt className='home__scroll-mouse' />
+            <span className='home__scroll-name'>Scroll down</span>
+            <UilArrowDown className='home__scroll-arrow' />
+          </a>
+        </div>
       </div>
     </section>
   );
