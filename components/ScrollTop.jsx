207fec03f1f9d51e7492cf9f10fd8835d140e253
NedduJS <nmamanipantoja@gmail.com>
2021-07-25 19:15:28 -0500

    Fixed window functions

--- components/ScrollTop.jsx
+++ components/ScrollTop.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { UilArrowUp } from '@iconscout/react-unicons';
 
 const ScrollTop = () => {
   const [scroll, setScroll] = useState('');
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY >= 360) {
-      setScroll('show-scroll');
-    } else {
-      setScroll('');
-    }
-  });
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 360) {
+        setScroll('show-scroll');
+      } else {
+        setScroll('');
+      }
+    });
+  }, []);
 
   return (
     <a href='#' className={`scrollup ${scroll}`} id='scroll-up'>
