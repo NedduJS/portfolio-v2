68c2c6c192f193ffbb377556ea5c474aab0d11e8
Nestor Mamani <nmamanipantoja@gmail.com>
2021-11-15 16:51:05 -0500

    Logo

--- components/About.jsx
+++ components/About.jsx
@@ -4,62 +4,65 @@ import { UilDownloadAlt } from '@iconscout/react-unicons';
 
 const About = () => {
   return (
-    <section className='about section' id='about'>
-      <h2 className='section__title'>About me</h2>
-      <span className='section__subtitle about__subtitle'>
+    <section className="about section" id="about">
+      <h2 className="section__title">About me</h2>
+      <span className="section__subtitle about__subtitle">
         A little bit of my history
       </span>
-      <div className='about__container container grid'>
-        <img src='' alt='' className='about__img' />
-        <div className='about__data'>
-          <p className='about__description'>
+      <div className="about__container container grid">
+        <img src="" alt="" className="about__img" />
+        <div className="about__data">
+          <p className="about__description">
             Filled by curiosity. Action bias. I aim creating value using
             technology and working on projects with purpose.
           </p>
-          <p className='about__description'>
-            <p className='about__subdescription'>
+          <p className="about__description">
+            <p className="about__subdescription">
               In my journey of learning new things, I had an idea which required
               me to make a website.
             </p>
-            <p className='about__subdescription'>
+            <p className="about__subdescription">
               So I started to study Web Development at Platzi. Nowadays I have
               done responsive websites using React and Next.
             </p>
-            <p className='about__subdescription'>
+            <p className="about__subdescription">
               To make the backend of the websites I have worked with Express and
               Django rest framework.
             </p>
-            <p className='about__subdescription'>
+            <p className="about__subdescription">
               To figure out the website design and functionalities I use the
               design process from user research, user histories, low and high
               fidelity design in Figma.
             </p>
-            <p className='about__subdescription'>
+            <p className="about__subdescription">
               And in order to make more powerful my applications, I am learning
               Data Science with{' '}
               <a
-                className='about__description--link'
-                target='_blank'
-                href='https://platzi.com/p/nestoredduardo/'>
+                className="about__description--link"
+                target="_blank"
+                href="https://platzi.com/p/nestoredduardo/"
+              >
                 Platzi
               </a>{' '}
               and{' '}
               <a
-                className='about__description--link'
-                target='_blank'
-                href='https://www.kaggle.com/nestoredduardo'>
+                className="about__description--link"
+                target="_blank"
+                href="https://www.kaggle.com/nestoredduardo"
+              >
                 Kaggle
               </a>
               .
             </p>
           </p>
-          <div className='about__buttons'>
+          <div className="about__buttons">
             <a
-              href='https://drive.google.com/file/d/1JTiwHRAxbri_fQeEY6rp4M7fX7tIjaP7/view?usp=sharing'
-              target='_blank'
-              className='button button--flex'>
+              href="https://drive.google.com/file/d/1JTiwHRAxbri_fQeEY6rp4M7fX7tIjaP7/view?usp=sharing"
+              target="_blank"
+              className="button button--flex"
+            >
               Watch CV
-              <UilDownloadAlt className='button__icon' />
+              <UilDownloadAlt className="button__icon" />
             </a>
           </div>
         </div>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,39 +6,46 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <title>Nestor Eduardo Mamani Pantoja</title>
-          <meta name='title' content='Nestor Eduardo Mamani Pantoja' />
+          <link rel="icon" href="/logo.svg" />
+
+          <meta name="title" content="Nestor Eduardo Mamani Pantoja" />
           <meta
-            name='description'
-            content='Filled by curiosity. Action bias. I aim creating value using technology and working on projects with purpose.'
+            name="description"
+            content="Filled by curiosity. Action bias. I aim creating value using technology and working on projects with purpose."
           />
 
-          <meta property='og:type' content='website' />
-          <meta property='og:url' content='https://nestoredduardo.me/' />
-          <meta property='og:title' content='Nestor Eduardo Mamani Pantoja' />
+          <link rel="apple-touch-icon" href="/logo.svg" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://nestoredduardo.me/" />
+          <meta property="og:title" content="Nestor Eduardo Mamani Pantoja" />
           <meta
-            property='og:description'
-            content='Filled by curiosity. Action bias. I aim creating value using technology and working on projects with purpose.'
+            property="og:description"
+            content="Filled by curiosity. Action bias. I aim creating value using technology and working on projects with purpose."
           />
           <meta
-            property='og:image'
-            content='https://nestoredduardo.me/MetaImage.jpg'
+            property="og:image"
+            content="https://nestoredduardo.me/MetaImage.jpg"
           />
 
-          <meta property='twitter:card' content='summary_large_image' />
-          <meta property='twitter:url' content='https://nestoredduardo.me/' />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://nestoredduardo.me/" />
           <meta
-            property='twitter:title'
-            content='Nestor Eduardo Mamani Pantoja'
+            property="twitter:title"
+            content="Nestor Eduardo Mamani Pantoja"
           />
           <meta
-            property='twitter:description'
-            content='Filled by curiosity. Action bias. I aim creating value using technology and working on projects with purpose.'
+            property="twitter:description"
+            content="Filled by curiosity. Action bias. I aim creating value using technology and working on projects with purpose."
           />
           <meta
-            property='twitter:image'
-            content='https://nestoredduardo.me/MetaImage.jpg'
+            property="twitter:image"
+            content="https://nestoredduardo.me/MetaImage.jpg"
           />
-          <meta name="google-site-verification" content="r_d0z9UvAooIVRe5ktDakJVqf8CGb9W1rweCN2WQWhM" />
+          <meta
+            name="google-site-verification"
+            content="r_d0z9UvAooIVRe5ktDakJVqf8CGb9W1rweCN2WQWhM"
+          />
         </Head>
         <body>
           <Main />
